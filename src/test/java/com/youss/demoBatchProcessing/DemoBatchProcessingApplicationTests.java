3ca0aa8d6e034d834e2581524152df9176a62e6b
youssoufalioum <youssoufalioum@yahoo.fr>
2025-01-27 11:37:42 +0200

    first commit

---
+++ src/test/java/com/youss/demoBatchProcessing/DemoBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youss.demoBatchProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
